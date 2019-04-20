2ed2565b6e641b9612460f0d02697079646fbf49
dongpani <epyruskdh@gmail.com>
2019-04-20 22:11:21 +0900

    데이터베이스와 통신하기

--- daily-report-project/src/components/Day.vue
+++ daily-report-project/src/components/Day.vue
@@ -5,7 +5,7 @@
       </h1>
 
       <ul class="day-list">
-        <li v-for="item in items" :key="item.id" class="day-list-item">
+        <li v-for="item in items" :key="item.id" class="day-list-item" :class="{'open' : item.open}">
            <div class="time">
              <h2>
                {{ item.id }}
@@ -13,15 +13,28 @@
            </div>
 
           <div class="action">
-            <input type="text" v-model="item.action" placeholder="한 일을 작성해주세요.">
+            <p v-if="!item.open" @click="toggleOpen(item)">
+              {{item.action}}
+              <span v-if="item.action.length === 0">내용을 작성해주세요.</span>
+            </p>
+            <input v-if="item.open" type="text" v-model="item.action" @keyup.enter="updateItem(item)" placeholder="한 일을 작성해주세요.">
           </div>
 
           <day-score @onUpdateScore="onUpdateScore" :item="item">
               
           </day-score>
 
-          <div class="note">
-            <textarea v-model="item.note" placeholder="노트를 작성해주세요."> </textarea>
+          <div class="note" v-if="item.open">
+            <textarea v-model="item.note" @keyup.enter="updateItem(item)" placeholder="노트를 작성해주세요."> </textarea>
+          </div>
+
+          <div class="buttons">
+            <button class="save" @click="updateItem(item)">
+              저장
+            </button>
+            <button class="cancel" @click="toggleOpen(item)">
+              취소
+            </button>
           </div>
 
         </li>
@@ -31,6 +44,8 @@
 
 <script>
 import DayScore from './DayScore'
+import axios from 'axios'
+import moment from 'moment'
 
 export default {
   name: 'Day',
@@ -42,38 +57,77 @@ export default {
   methods:{
     onUpdateScore(item, score) {
         item.score = score;
+        this.updateItem(item);
+    },
+
+    getItems() {
+      let url = `https://new-daily-report-34671.firebaseio.com/items.json?orderBy="$key"&startAt="${this.$route.params.date}"&endAt="${this.$route.params.date}-22-24"`;
+
+      axios.get(url).then((res) => {
+        this.items = this.displayItems(res.data);
+      });
+    },
+
+    displayItems(result) {
+        // console.log(result);
+        let items = [];
+        let startTime = '08:00'; // 시작 시간
+
+        for (let i=0; i < 12; i++) {
+            let datetime = moment(this.$route.params.date+ ' ' + startTime);
+            let itemKey = `${this.$route.params.date}-${datetime.add(i*2, 'hours').format('HH')}-${datetime.add(2, 'hours').format('HH')}`;
+
+            let item = {
+              id: itemKey,
+              action : '',
+              note: '',
+              score: undefined
+            }
+            
+            Object.keys(result).map((key) => {
+                if(key === itemKey) {
+                  item = result[key]
+                }
+            });
+
+            items.push(item);
+        }
+
+        return items;
+
+    },
+
+    updateItem(item) {
+        console.log('update', item);
+        let url = `https://new-daily-report-34671.firebaseio.com/items/${item.id}.json`;        
+        axios.put(url, item).then((res) => {
+            this.getItems();
+        });
+    },
+
+    toggleOpen(item) {
+        item.open = !item.open;
     }
+
   },
   computed : {},
   data() {
       return{
-        items : [
-          {
-            action: "", 
-            id: "2018-04-21-08-10", 
-            note: "", 
-            open: false, 
-            score: undefined 
-          },
-          {
-            action: "", 
-            id: "2018-04-21-10-12", // 날짜. id, key 역할
-            note: "", // 노트
-            open: false, // 편집모드
-            score: undefined // 점수
-          },
-          {
-            action: "", // 한 일 
-            id: "2018-04-21-12-14", // 날짜. id, key 역할
-            note: "", // 노트
-            open: false, // 편집모드
-            score: undefined // 점수
-          }                    
-        ]
+        items : []
       }
   },
 
-  props: { }
+  props: { },
+
+  created() {
+    this.getItems();
+  },
+
+  watch: {
+    '$route' (to, from) {
+      this.getItems();
+    }
+  }
 
 }
 
